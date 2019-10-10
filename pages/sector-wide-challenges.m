27e9eb0a903ad88c2f83936b93dd12d52dd84b98
travispoh <49739266+travispoh@users.noreply.github.com>
2019-10-10 14:54:45 +0800

    Create sector-wide-challenges.m

---
+++ pages/sector-wide-challenges.m
@@ -0,0 +1,11 @@
+---
+layout: simple-page
+title: Sector-Wide Challenges
+permalink: /sector-wide-challenges/
+breadcrumb: Sector-Wide Challenges
+---
+To further support the Whole-Of-Government (WOG) Digital Transformation and Innovation efforts, sector-level challenges are being launched with the objective of scaling innovative solutions coming out of open innovation for industry-wide adoption. Such challenges are championed by sector leads and their representative sector members, who will help to propagate and commercialise solutions to benefit the wider industry.
+ 
+Sector-level digitalisation challenges are now launched through IMDA’s Open Innovation Platform (OIP). The OIP is a virtual crowd-sourcing platform that connects and matches real business challenges or digitalisation opportunities of Problem Owners to Problem Solvers. Since July 2018, the platform has worked with corporates and government agencies to launch 67 challenges with S$2M prize monies to bridge Problem Owner business needs with digital solutions.  
+
+Below are challenges that are common to their sector:
