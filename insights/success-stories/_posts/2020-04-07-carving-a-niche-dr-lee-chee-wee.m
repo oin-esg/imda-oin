8a503a08925681112d9958ba57180f99dcbb05c5
travispoh <49739266+travispoh@users.noreply.github.com>
2020-04-07 12:31:46 +0800

    Create 2020-04-07-carving-a-niche-dr-lee-chee-wee.m

---
+++ insights/success-stories/_posts/2020-04-07-carving-a-niche-dr-lee-chee-wee.m
@@ -0,0 +1,35 @@
+---
+layout: post
+title: Carving a Niche—An Interview with Dr Lee Chee Wee of the Aquaculture Innovation Centre 
+date: 2020-04-07
+permalink: /insights/success-stories/carving-a-niche-dr-lee-chee-wee/
+---
+Fishing for solutions in a sea of innovation
+<sub>The Aquaculture Innovation Centre is looking to support and grow the aquaculture industry in Singapore through collaboration and open innovation.</sun>
+
+<p align="justify">As more of the global population moves towards urban centres, land originally used for food production is rapidly being converted into urban sprawl. The city-state of Singapore, for example, uses less than 1% of its total land area for agriculture, relying mostly on overseas sources to sustain its food supply. In times of worldwide uncertainty, this dependence on the global supply chain can pose a food security issue if food-producing nations shut their borders.<br>
+To mitigate this potential risk, the government of Singapore launched an ambitious goal in March 2019: to produce 30% of the country’s nutritional needs domestically by 2030. Called ‘30 by 30,’ the plan involves looking into aquaculture as an area of protein production, taking advantage of Singapore’s status as an island nation.<br>
+To help traditional aquaculture farmers adopt new technologies and work towards the nationwide goal, the Aquaculture Innovation Centre (AIC) was set up in 2019 at Temasek Polytechnic. AIC aims to link companies to its large network of technology partners and experts to develop solutions through an open innovation framework. Speaking with IPI, Dr Lee Chee Wee, Centre Director of AIC, shared how companies and technology providers can partner with AIC to make Singapore a hub for aquaculture innovation.<br><br>
+
+<b>1. What does the Aquaculture Innovation Centre (AIC) do and what are the key capabilities and areas you focus on?</b><br>
+As a Centre of Innovation (COI) funded by Enterprise Singapore, our objective is to grow both local and to certain extent regional aquaculture enterprises. Specifically, we aim to develop innovative solutions in emerging areas like sustainable, super-intensive urban and smart aquaculture to fuel greater farming productivity and environmental sustainability.<br>
+In this respect, AIC has several key strategic areas in which we focus on. For example, we research on the formulation of feeds because farming in super-intensive urban aquaculture systems like Singapore’s has very different nutritional requirements. Besides nutrition, we also focus on other areas like health and disease management, breeding and seed production, waste management and emerging technologies like sensors, Internet of Things (IoT), robotics and artificial intelligence (AI).<br><br>
+
+<b>2. What is unique about AIC’s approach to open innovation?</b><br>
+The AIC is the first COI to adopt an inclusive and collaborative approach to open innovation, namely, through a consortium of its members that include nine research institutes, agencies, universities and polytechnics in Singapore. With this organisational structure, AIC possesses a vast pool of experts in different areas that can be drawn upon to lend their expertise.<br>
+This talent resource is important because we can consult different experts for different problems. For example, a company might be looking for ways to improve the digestibility of a particular feed. AIC can invite relevant experts, such as nutritionists, biochemists and even microbiologists, to provide their specific insights for developing a solution.<br>
+Other than that, the approach AIC takes also makes sure that the solutions it provides are robust, practical and financially viable to the companies. Not only do we consider the feasibility of the solution, we also review the results at each stage of the project before proceeding, ensuring that our solutions are built on a strong foundation of knowledge. Collectively, this unique approach to open innovation enables us to develop solutions that we believe are best for companies seeking our help.<br><br>
+
+<b>3. Kindly share some examples of successful collaborations and how working with AIC has benefitted these collaborators.</b><br>
+As a bridge between the public and private sector, we engage in both internal and external collaboration. This open innovation approach has helped to create a vibrant collaborative landscape, which has been very successful as well.<br>
+One example of a collaboration that I would like to highlight is with an internal partner. We were engaged by the College of Engineering at Nanyang Technological University, who were keen to do research in aquaculture but lacked the necessary infrastructure. We were able to provide them with not just the relevant facilities, but also problem statements posed by the industry to guide their research. With their technical expertise and our resources, the resulting collaboration created several intellectual properties (IP) and was successful at solving many technical problems posed to us by our external collaborators.<br><br>
+
+<b>4. What kinds of companies or collaborators are you looking to work with?</b><br>
+We are looking for collaborators that can contribute either their knowledge or their resources and IPs. For the first group, we welcome them to be a part of our community of experts. Contributors in this respect do not need to specialise in aquaculture as we want to maintain a diverse pool of experts in different areas. For example, someone knowledgeable in human disease could potentially advise us on disease management in fish.<br>
+In addition, we are also keen to engage collaborators with aquaculture resources, such as foreign COIs or institutes. This type of collaboration can come in many forms, such as research and development (R&D), services, training or education. For example, we have collaborators in India that will help aquaculture enterprises in Singapore deploy their technology there, where greater operational support and space for growth can be found.<br><br>
+
+<b>5. How can companies engage and begin working with AIC?</b><br>
+Companies may engage us through our website and various outreach events. While we are only nine months old, AIC has met with more than 250 companies to date. Companies may also engage us through agencies like the Enterprise Singapore, Economic Development Board or the Singapore Food Agency. By collaborating with AIC, your company will be part of a nationwide effort to grow the aquaculture industry, so that we can help Singapore become the global hub for aquaculture innovation.<br><br></p>
+
+Copyright: IPI Singapore. Read the original article [here](https://www.ipi-singapore.org/innovation-insights/carving-niche—-interview-dr-lee-chee-wee-aquaculture-innovation-centre){:target="_blank"} 
+
